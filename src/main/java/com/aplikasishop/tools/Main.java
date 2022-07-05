d8562b58df5e67810f2c6d35e6ea7640cad88895
Luis Ginanjar <luis@sahabatbisnis.co.id>
2022-07-05 10:00:43 +0700

    refactoring

--- src/main/java/com/aplikasishop/tools/Main.java
+++ src/main/java/com/aplikasishop/tools/Main.java
@@ -3,18 +3,57 @@ package com.aplikasishop.tools;
 import com.aplikasishop.tools.interfaces.FileOperation;
 import com.aplikasishop.tools.interfaces.Watcher;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        var ref = new Object() {
-            String source = "";
-            final List<String> destination = new ArrayList<>();
-        };
+    public static void main(String[] args) {
+        try {
+            var valueParam = getValueParam(args);
 
+            printParam(valueParam);
+
+            var watcher = getWatcher();
+            var fileOperation = getFileOperation();
+            watcher.startWatch(valueParam.source, file -> valueParam.destination.forEach(dest -> {
+                onFolderChange(fileOperation, file, dest);
+            }));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static FileOperation getFileOperation() {
+        return new AsFileOperation();
+    }
+
+    private static Watcher getWatcher() {
+        return new AsFolderWatcher();
+    }
+
+    private static void printParam(Param valueParam) {
+        System.out.println("Source: " + valueParam.source);
+        valueParam.destination.forEach(dest -> System.out.println("Destination: " + dest));
+    }
+
+    private static void onFolderChange(FileOperation fileOperation, File file, String dest) {
+        try {
+            while (true) {
+                try {
+                    fileOperation.CopyFile(file.getAbsolutePath(), dest + "/" + file.getName());
+                    break;
+                } catch (IOException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+    }
+
+    private static Param getValueParam(String[] args) {
+        var ref = new Param();
         Arrays.stream(args).forEach(arg -> {
             if (arg.startsWith("--source=")) {
                 var value = arg.trim().split("=");
@@ -29,21 +68,6 @@ public class Main {
             }
         });
 
-        System.out.println("Source: " + ref.source);
-        ref.destination.forEach(dest -> System.out.println("Destination: " + dest));
-
-        Watcher watcher = new AsFolderWatcher();
-        FileOperation fileOperation = new AsFileOperation();
-        watcher.startWatch(ref.source, file -> ref.destination.forEach(dest -> {
-            while (true) {
-                try {
-                    fileOperation.CopyFile(file.getAbsolutePath(),dest + "/" + file.getName() );
-                    break;
-                } catch (IOException e) {
-                    System.out.println("Error: " + e.getMessage());
-                }
-            }
-
-        }));
+        return ref;
     }
 }
---
+++ src/main/java/com/aplikasishop/tools/Param.java
@@ -0,0 +1,9 @@
+package com.aplikasishop.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Param {
+    public final List<String> destination = new ArrayList<>();
+    public String source = "";
+}
