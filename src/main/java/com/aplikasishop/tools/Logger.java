98d808dffa0242ca82cf6ce87f8e0ed254dff35c
Luis Ginanjar <luis@sahabatbisnis.co.id>
2023-10-23 15:23:28 +0700

    refactor logger

--- src/main/java/com/aplikasishop/tools/Logger.java
+++ src/main/java/com/aplikasishop/tools/Logger.java
@@ -6,20 +6,17 @@ public class Logger {
         System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
     }
 
-    public static void logWhite(String message) {
-        System.out.println("\u001B[37m" + message + "\u001B[0m");
-    }//function show verbose with red green color without Logger
-
     public static void log(String message) {
         System.out.print(message);
-    }//f
+    }
+
     public static void logGreen(String message) {
         System.out.println("\u001B[32m" + message + "\u001B[0m");
     }
 
     public static void logYellow(String message) {
         System.out.println("\u001B[33m" + message + "\u001B[0m");
-    }//function show verbose with blue font color without Logger
+    }
 
     public static void logBlue(String message) {
         System.out.println("\u001B[34m" + message + "\u001B[0m");
